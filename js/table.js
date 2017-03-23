3abdad43887feb8f6ebafbdaed15d435942806af
Linux <linux@ubuntu1204x64.(none)>
2017-03-23 16:29:16 +0800

    init

---
+++ js/table.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-03-16 15:46:41
+ * @version $Id$
+ */
+function function1(e){
+	document.getElementById("table1").style.backgroundColor="red";
+}
+function function2(e){
+	document.getElementById("table2").insertRow(2);
+}
